1b31c5b21a93d378af48a339f737a6d5d878001f
Pitla Sai Krishna <108579133+Pitlakrishna@users.noreply.github.com>
2024-05-01 20:42:32 +0530

    server.js

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ const __dirname = path.dirname(__filename);
 const app = express()
 
 
-app.use(cors())
+app.use(cors({
+          origin : ["https://deploy-mern-"]
+     }))
 app.use(express.json())
 app.use(morgan("dev"))
 app.use(express.static(path.join(__dirname, "./client/build")))
