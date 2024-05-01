04512838845844ba51ae892a1ddd23395dc2cd3e
Pitla Sai Krishna <ksai94064@gmail.com>
2024-05-01 17:42:01 +0530

    MERN todo Application
    MERN Full stack TODO Application

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from "express"
+import dotEnv from "dotenv"
+import cors from "cors"
+import colors from "colors"
+import ConnectDB from "./config/db.js"
+import morgan from "morgan"
+import authRoute from "./Routes/authRoute.js"
+// import path from 'path'
+// import { fileURLToPath } from "url";
+
+
+dotEnv.config()
+ConnectDB()
+const app = express()
+
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan("dev"))
+// app.use(express.static(path.join(__dirname, "./client/build")))
+
+app.use('/api/v1/task', authRoute)
+
+// app.use('*', function (req, res) {
+//     res.sendFile(path.join(__dirname, './client/build/index.html'))
+// })
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server connected to port 8080 ..     `.bgCyan.white)
+})
